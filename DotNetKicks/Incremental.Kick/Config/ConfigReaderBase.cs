7883ba23c478545256cc87314901ebcd8d39420c
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-07-04 17:10:32 +0000

    Removed some exception handling

--- DotNetKicks/Incremental.Kick/Config/ConfigReaderBase.cs
+++ DotNetKicks/Incremental.Kick/Config/ConfigReaderBase.cs
@@ -9,21 +9,11 @@ namespace Incremental.Kick.Config {
 
     public abstract class ConfigReaderBase {
         protected static object Read(string configSection) {
-            try {
-                return ConfigurationManager.Read(configSection);
-            } catch(Exception exception) {
-                Trace.TraceError(string.Format("Could not load the '{0}' configuration file", configSection));
-                throw;
-            }
+            return ConfigurationManager.Read(configSection);
         }
 
         protected static Hashtable GetHashtable(string configSection) {
-            try {
-                return ConfigurationManager.Read(configSection) as Hashtable;
-            } catch(Exception exception) {
-                Trace.TraceError(string.Format("Could not load the '{0}' configuration file Hashtable", configSection));
-                throw;
-            }
+            return ConfigurationManager.Read(configSection) as Hashtable;
         }
     }
 }
