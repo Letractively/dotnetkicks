bbf8851767c8feae5c7ce28de955fa2723b59d49
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-07-04 17:02:19 +0000

    Added static files

---
+++ DotNetKicks/Incremental.Kick.Web.UI/Static/Scripts/2.0.1/Admin/Kick.js
@@ -0,0 +1,22 @@
+﻿
+function SetAutoKickCount(storyID) {
+    var autoKickCount = prompt('Please enter an auto kick count for this storyID:' + storyID,'6')
+    
+    if(autoKickCount > -1) {
+        Ajax_SetAutoKickCount(storyID, autoKickCount, SetAutoKickCount_End, Failure);
+    }
+}
+
+function SetAutoKickCount_End(storyID, autoKickCount) {
+    alert("The autoKickCount is " + autoKickCount);
+}
+
+function Ajax_SetAutoKickCount(storyID, autoKickCount, success, failure) {   
+    dojo.io.bind({
+        url: AJAX_BASE_URL + "/autokickit/" + storyID + "/" + autoKickCount,
+        load: function(type, data, evt) { success(storyID, autoKickCount); },
+        error: function(type, data, evt) { failure(); },
+        mimetype: "text/plain"
+    });
+}
+
