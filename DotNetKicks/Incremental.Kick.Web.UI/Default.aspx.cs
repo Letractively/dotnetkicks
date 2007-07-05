4a07ff69b847152e1ce4fc1d293d06a476500161
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-07-05 18:25:16 +0000

    Continued DAL/BLL implementation

--- DotNetKicks/Incremental.Kick.Web.UI/Default.aspx.cs
+++ DotNetKicks/Incremental.Kick.Web.UI/Default.aspx.cs
@@ -11,8 +11,8 @@ using System.Web.UI.HtmlControls;
 
 namespace Incremental.Kick.Web.UI {
     public partial class _Default : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-
+        protected void Page_Init(object sender, EventArgs e) {
+            Server.Transfer("~/Pages/Home.aspx"); //TEMP: only for dev environment
         }
     }
 }
