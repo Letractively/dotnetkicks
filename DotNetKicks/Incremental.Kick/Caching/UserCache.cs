0e663d122d17831b79f75e851e80faa578a56e51
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-07-06 09:17:40 +0000

    Kicking/Unkicking is now working

--- DotNetKicks/Incremental.Kick/Caching/UserCache.cs
+++ DotNetKicks/Incremental.Kick/Caching/UserCache.cs
@@ -5,12 +5,9 @@ using Incremental.Kick.Dal;
 using Incremental.Kick.Security;
 using Incremental.Kick.BusinessLogic;
 
-namespace Incremental.Kick.Caching
-{
-    public class UserCache
-    {
-        public static User GetUser(string securityToken)
-        {
+namespace Incremental.Kick.Caching {
+    public class UserCache {
+        public static User GetUser(string securityToken) {
             int? userID = null;
 
             if (!String.IsNullOrEmpty(securityToken))
@@ -26,16 +23,13 @@ namespace Incremental.Kick.Caching
         }
 
         private static object _getUserLock = new object();
-        private static User GetUser(int userID)
-        {
+        private static User GetUser(int userID) {
             CacheManager<string, User> userCache = GetUserCache();
             string cacheKey = GetUserProfileCacheKey(userID);
             User user = userCache[cacheKey];
 
-            lock (_getUserLock)
-            {
-                if (user == null)
-                {
+            lock (_getUserLock) {
+                if (user == null) {
                     if (userID == 0) {
                         //TODO: GJ: construct an anonymous user object
                         user = new User(0);
@@ -52,14 +46,12 @@ namespace Incremental.Kick.Caching
         }
 
 
-        public static int GetUserID(string username)
-        {
+        public static int GetUserID(string username) {
             CacheManager<string, int?> userIDCache = GetUserIDCache();
             string cacheKey = "GetUserID_" + username;
 
             int? userID = userIDCache[cacheKey];
-            if (!userID.HasValue)
-            {
+            if (!userID.HasValue) {
                 userID = User.FetchUserByUsername(username).UserID;
                 System.Diagnostics.Trace.Write("Cache: inserting [" + cacheKey + "]");
                 userIDCache.Insert(cacheKey, userID, 500); //TODO: Config
@@ -67,97 +59,66 @@ namespace Incremental.Kick.Caching
 
             return userID.Value;
         }
-        
-        public static void RemoveUser(string securityToken)
-        {
+
+        public static void RemoveUser(string securityToken) {
             GetUserCache().Remove(GetUserProfileCacheKey(SecurityToken.FromString(securityToken).UserID));
         }
 
-        private static string GetUserProfileCacheKey()
-        {
+        private static string GetUserProfileCacheKey() {
             return "UserProfile_Anonymous";
         }
 
-        private static string GetUserProfileCacheKey(int userID)
-        {
+        private static string GetUserProfileCacheKey(int userID) {
             return "UserProfile_" + userID;
         }
 
-        //TODO: GJ: some major improvements are needed here.
-        public static int KickStory(int storyID, int userID, int hostID)
-        {
-            ////TODO: GJ: implement
-            //StoryKick storyKick = StoryBR.AddStoryKick(storyID, userID, hostID);
-    
-            //    //merge with the cache
-            //    GetUserStoryKicks(userID).Merge(storyKickTable);
-                 
+        //TODO: GJ: some improvements are needed here - a sproc would be better
+        public static int KickStory(int storyID, int userID, int hostID) {
+            StoryKick storyKick = StoryBR.AddStoryKick(storyID, userID, hostID);
+            GetUserStoryKicks(userID).Add(storyKick);
 
-            //    //increment the story kick count in the db (could be a db trigger?)
-            //    Kick_StoryDataSet storyDS = new Kick_StoryBR().GetByStoryID(storyID);
-            //    storyDS.Kick_Story[0].KickCount++;
-            //    new Kick_StoryBR().Persist(storyDS);
-
-            //    return storyDS.Kick_Story[0].KickCount; 
-            return 999;
-          
+            //increment the story kick count in the db (could be a db trigger?)
+            return StoryBR.IncrementKickCount(storyID);
         }
 
-        public static int UnKickStory(int storyID, int userID, int hostID)
-        {
-            //TODO: GJ: implement
+        public static int UnKickStory(int storyID, int userID, int hostID) {
             StoryBR.DeleteStoryKick(storyID, userID, hostID);
-            /*
 
-                //now remove from the cache
-                RemoveStoryKick(storyID, userID, hostID);
+            //now remove from the cache
+            RemoveStoryKick(storyID, userID, hostID);
 
-                //decrement the story kick count in the db (could be a trigger?)
-                Kick_StoryDataSet storyDS = new Kick_StoryBR().GetByStoryID(storyID);
-                storyDS.Kick_Story[0].KickCount--;
-                new Kick_StoryBR().Persist(storyDS);
-
-                return storyDS.Kick_Story[0].KickCount;
-            */
-            return 999;
+            return StoryBR.DecrementKickCount(storyID);
         }
 
-        public static bool HasUserKickedStory(int storyID, int userID)
-        {
-            //PERF: there will be huge performance benefits if we use a hashtable here
-            /*Kick_StoryKickTable storyKickTable = GetUserStoryKicks(userID);
-
-            foreach (Kick_StoryKickRow storyKick in storyKickTable)
-            {
+        public static bool HasUserKickedStory(int storyID, int userID) {
+            //PERF: there will be performance benefits if we use a hashtable here
+            foreach (StoryKick storyKick in GetUserStoryKicks(userID)) {
                 if (storyID == storyKick.StoryID)
                     return true;
-            }*/
+            }
 
             return false;
         }
 
-        public static void RemoveStoryKick(int storyID, int userID, int hostID)
-        {
-           /* Kick_StoryKickTable storyKickTable = GetUserStoryKicks(userID);
-            Kick_StoryKickRow storyKick = storyKickTable.GetRowByID(storyID, userID, hostID);
-            try
-            {
-                storyKick.Delete();
-                storyKickTable.AcceptChanges();
+        public static void RemoveStoryKick(int storyID, int userID, int hostID) {
+            //PERF: there will be performance benefits if we use a hashtable here
+            StoryKickCollection storyKicks = GetUserStoryKicks(userID);
+            foreach (StoryKick storyKick in storyKicks) {
+                if (storyID == storyKick.StoryID) {
+                    storyKicks.Remove(storyKick);
+                    return;
+                }
             }
-            catch { }*/
         }
 
-        public static StoryKickCollection GetUserStoryKicks(int userID)
-        {
+        public static StoryKickCollection GetUserStoryKicks(int userID) {
             string cacheKey = String.Format("Kick_StoryKickTable_{0}", userID);
 
             CacheManager<string, StoryKickCollection> storyKickCache = GetStoryKickCache();
 
             StoryKickCollection storyKicks = storyKickCache[cacheKey];
 
-            if (storyKicks == null)
-            {
+            if (storyKicks == null) {
                 //TODO: get the latest n kicks for this userIdentifier
                 storyKicks = StoryKickBR.GetAllByUserID(userID);
                 System.Diagnostics.Trace.Write("Cache: inserting [" + cacheKey + "]");
@@ -169,18 +130,15 @@ namespace Incremental.Kick.Caching
 
 
 
-        private static CacheManager<string, StoryKickCollection> GetStoryKickCache()
-        {
+        private static CacheManager<string, StoryKickCollection> GetStoryKickCache() {
             return CacheManager<string, StoryKickCollection>.GetInstance();
         }
 
-        private static CacheManager<string, User> GetUserCache()
-        {
+        private static CacheManager<string, User> GetUserCache() {
             return CacheManager<string, User>.GetInstance();
         }
 
-        private static CacheManager<string, int?> GetUserIDCache()
-        {
+        private static CacheManager<string, int?> GetUserIDCache() {
             return CacheManager<string, int?>.GetInstance();
         }
     }
