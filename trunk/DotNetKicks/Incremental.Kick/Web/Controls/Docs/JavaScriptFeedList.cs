425f37ff3861ba97372caab09880f7c0090a3195
simone.busoli <simone.busoli@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-04 00:03:36 +0000

    Add dotnetkicks to your site: configure number of links provided by embeddable javascript Edited "Add DotNetKicks.com news to your site" page to document the ability to limit the number of returned kicks. Edited the JavaScriptFeedList.cs control to expose the number of entries to show and the title as public properties, so that they can be customized. Issue 34

--- trunk/DotNetKicks/Incremental.Kick/Web/Controls/Docs/JavaScriptFeedList.cs
+++ trunk/DotNetKicks/Incremental.Kick/Web/Controls/Docs/JavaScriptFeedList.cs
@@ -1,15 +1,33 @@
 using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Web;
-using Incremental.Kick.Caching;
+using System.Web.UI;
 
+namespace Incremental.Kick.Web.Controls
+{
+    public class JavaScriptFeedList : KickHtmlControl
+    {
+        private int entryCount = 0;
+        private string title = "Homepage Feed";
 
-namespace Incremental.Kick.Web.Controls {
-    public class JavaScriptFeedList : KickHtmlControl {
-        
-        protected override void Render(System.Web.UI.HtmlTextWriter writer) {
-            this.RenderJavaScriptFeed("Homepage Feed", writer);
+        [DefaultValue(0)]
+        public int EntryCount
+        {
+            get { return entryCount; }
+            set { entryCount = value; }
+        }
+
+        [DefaultValue("Homepage Feed")]
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            RenderJavaScriptFeed(writer);
 
             //Kick_CategoryTable categoryTable = CategoryCache.GetCategories(this.KickPage.HostProfile.HostID);
             //foreach (Kick_CategoryRow category in categoryTable) {
@@ -17,24 +35,32 @@ namespace Incremental.Kick.Web.Controls {
             //}
         }
 
-        private void RenderJavaScriptFeed(string name, System.Web.UI.HtmlTextWriter writer) {
-            this.RenderJavaScriptFeed(name, "", writer);
+        private void RenderJavaScriptFeed(HtmlTextWriter writer)
+        {
+            RenderJavaScriptFeed("", writer);
         }
 
+        private void RenderJavaScriptFeed(string category, HtmlTextWriter writer)
+        {
+            StringBuilder javascriptUrl = new StringBuilder(KickPage.HostProfile.RootUrl);
 
-        private void RenderJavaScriptFeed(string title, string category, System.Web.UI.HtmlTextWriter writer) {
-            string javascriptUrl = this.KickPage.HostProfile.RootUrl;
             if(!String.IsNullOrEmpty(category))
-                javascriptUrl += "/" + category;
-            javascriptUrl += "/feeds/js";
+                javascriptUrl.Append("/").Append(category);
 
-            string script = String.Format(@"<script src=""{0}"" type=""text/javascript"" language=""javascript""></script>",
-                javascriptUrl);
-            
-            writer.WriteLine(@"
+            javascriptUrl.Append("/feeds/js");
+
+            if(entryCount != 0)
+                javascriptUrl.AppendFormat("?count={0}", entryCount);
+
+            string script =
+                String.Format(@"<script src=""{0}"" type=""text/javascript"" language=""javascript""></script>", javascriptUrl);
+
+            writer.WriteLine(
+                @"
                 <p><strong>{0}</strong><br />
-                <textarea rows=""1"" cols=""80"">{1}</textarea></p>
-            ", title, HttpUtility.HtmlEncode(script));
+                <textarea rows=""1"" cols=""80"" onclick=""this.select();"">{1}</textarea></p>
+            ",
+                title, HttpUtility.HtmlEncode(script));
         }
     }
 }
