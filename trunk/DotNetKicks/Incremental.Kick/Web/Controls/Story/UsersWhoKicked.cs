b32ec96859868b535d28b31f146fdc878894c929
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-08 15:27:03 +0000

    Updated the Who Kicked control to reuse the UserLink control

--- trunk/DotNetKicks/Incremental.Kick/Web/Controls/Story/UsersWhoKicked.cs
+++ trunk/DotNetKicks/Incremental.Kick/Web/Controls/Story/UsersWhoKicked.cs
@@ -9,49 +9,31 @@ using Incremental.Kick.Helpers;
 using Incremental.Kick.Dal.Entities;
 using System.Web;
 
-namespace Incremental.Kick.Web.Controls
-{
-    public class UsersWhoKicked : KickWebControl
-    {
+namespace Incremental.Kick.Web.Controls {
+    public class UsersWhoKicked : KickWebControl {
 
         private UserCollection _users;
 
-        public void DataBind(UserCollection users)
-        {
+        public void DataBind(UserCollection users) {
             this._users = users;
         }
 
-        protected override void Render(HtmlTextWriter writer)
-        {
-            StringBuilder sb = new StringBuilder();
+        protected override void Render(HtmlTextWriter writer) {
+            writer.Write(@"<br /><div class=""PageSmallCaption"">Users who kicked this story:</div>");
 
-            sb.Append(@"<br /><div class=""PageSmallCaption"">Users who kicked this story:</div>");
+            writer.Write(@"<div class=""UsersWhoKicked"">");
 
-            sb.Append(@"<div class=""UsersWhoKicked"">");
-
-            if(_users.Count == 0)
-            {
-                sb.Append("Hmm...no users have kicked this story yet? Not possible. Must be a bug, sorry.");
-            }
-            else
-            {
-                int count = _users.Count;
-
-                for (int i = 0; i < count; i++)
-                {
-                    string anchor = "<a href=\"{0}\" title=\"Views {1}\">{1}</a>";
-                    string username = _users[i].Username;
-                    string url = UrlFactory.CreateUrl(UrlFactory.PageName.UserHome, username); 
-                    sb.Append(String.Format(anchor,url,username));
-
-                    if (i + 1 != count) sb.Append(", ");
+            if (_users.Count == 0) {
+                writer.Write("Hmm...no users have kicked this story yet? Not possible. Must be a bug, sorry.");
+            } else {
+                UserLink userLink = new UserLink();
+                foreach (User user in _users) {
+                    userLink.DataBind(user);
+                    userLink.RenderControl(writer);
                 }
-               
             }
 
-            sb.Append("</div><br/>");
-            writer.WriteLine(sb.ToString());
-
+            writer.Write("</div><br/>");
         }
     }
 }
