7353296ea6d7295a559cc6d8c18724ccc3c541b7
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-08 10:08:12 +0000

    Updated some login links to support the new Url redirecting feature introduced by Simone in SVN revision 193

--- DotNetKicks/Incremental.Kick.Web.UI/Pages/User/LoginSwitch.aspx.cs
+++ DotNetKicks/Incremental.Kick.Web.UI/Pages/User/LoginSwitch.aspx.cs
@@ -1,13 +1,4 @@
 using System;
-using System.Data;
-using System.Configuration;
-using System.Collections;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 using Incremental.Kick.Web.Helpers;
 
 namespace Incremental.Kick.Web.UI.Pages.User {
@@ -16,16 +7,10 @@ namespace Incremental.Kick.Web.UI.Pages.User {
             if (this.KickUserProfile.IsGeneratedPassword) {
                 Response.Redirect(UrlFactory.CreateUrl(UrlFactory.PageName.ChangePassword));
             } else {
-                // TODONE: get the URL from the querystring. 
                 // No need to check if the querystring parameter is null or empty
                 // because if it is the root url is returned
                 Response.Redirect(Request.QueryString["url"]);
-
-                /*Response.Write(HttpUtility.UrlDecode(Request["AA"]));
-                Response.Write(this.Request.QueryString + "<br/>");
-                Response.Write(this.Request.RawUrl + "<br/>");*/
             }
-
         }
     }
 }
