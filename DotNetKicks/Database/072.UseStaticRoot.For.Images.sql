e9a5dda1d428374e58440e86960e45aa574a594c
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-10 09:18:04 +0000

    This has already been added, it was renamed to '008.UseStaticRoot.For.Images.sql'

--- DotNetKicks/Database/072.UseStaticRoot.For.Images.sql
+++
@@ -1,7 +0,0 @@
---SQL file UseStaticRoot
---==============================================
---Allows a site to use  static root for images or have them as a folder under root
- 
---Issue Number 72
-
-ALTER TABLE [dbo].[Kick_Host] ADD [UseStaticRoot] [bit] NULL
