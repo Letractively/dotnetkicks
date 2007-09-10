3631dd1802983511a6832bc3a7cf943930540c2b
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-10 09:15:31 +0000

    Setting page title to 'Who is online?'

--- trunk/DotNetKicks/Incremental.Kick.Web.UI/Pages/Community/WhoIsOnline.aspx.cs
+++ trunk/DotNetKicks/Incremental.Kick.Web.UI/Pages/Community/WhoIsOnline.aspx.cs
@@ -14,6 +14,7 @@ namespace Incremental.Kick.Web.UI.Pages.Community {
     public partial class WhoIsOnline : Incremental.Kick.Web.Controls.KickUIPage {
         protected void Page_Load(object sender, EventArgs e) {
             this.DisplayAds = false;
+            this.Title = this.HostProfile.SiteTitle + " : Who is online?";
 
             this.UserOnlineList.DataBind(UserCache.GetOnlineUsers(30, this.HostProfile.HostID));
             this.UserTodayList.DataBind(UserCache.GetOnlineUsers(1440, this.HostProfile.HostID));
