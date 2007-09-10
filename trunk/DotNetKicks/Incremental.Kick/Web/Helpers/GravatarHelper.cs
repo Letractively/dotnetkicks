51b800ceb44ad33dfe4305c08702ef51ef5a0ef5
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-10 15:11:55 +0000

    Improved the gravatar caching - a copy is now served while a fresh version is being downloaded. This does need some refactoring, but it seems to be working well. There is a race condition that needs to be solved at some point too.

--- trunk/DotNetKicks/Incremental.Kick/Web/Helpers/GravatarHelper.cs
+++ trunk/DotNetKicks/Incremental.Kick/Web/Helpers/GravatarHelper.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Incremental.Kick.Helpers;
+using System.IO;
 
 namespace Incremental.Kick.Web.Helpers {
     public class GravatarHelper {
@@ -10,9 +11,18 @@ namespace Incremental.Kick.Web.Helpers {
             AsyncHelper.FireAndForget(new DownloadGravatarDelegate(DownloadGravatar), gravatarID, size, targetFolderPath);
         }
 
+        private static object _downloadLock = new object();
+
         public static void DownloadGravatar(string gravatarID, int size, string targetFilePath) {
             string gravatarPath = String.Format("http://www.gravatar.com/avatar.php?gravatar_id={0}&size={1}", gravatarID, size);
-            HttpHelper.DownloadFile(gravatarPath, targetFilePath);
+
+            if (!File.Exists(targetFilePath)) //TODO: GJ: there is a possible race condition here
+                HttpHelper.DownloadFile(gravatarPath, targetFilePath);
+
+            try { //Delete copy as we now have a fresh copy
+                File.Delete(targetFilePath.Replace(".jpg", ".copy.jpg"));
+            } catch (System.IO.IOException) { }
+            
         }
     }
 }
