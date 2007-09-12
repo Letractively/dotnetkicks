4328ca27b4d023020227bbf14921e6123e60d8e8
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-12 15:40:48 +0000

    Added simple Database test

--- trunk/DotNetKicks/Incremental.Kick.Tests/SanityTests.cs
+++ trunk/DotNetKicks/Incremental.Kick.Tests/SanityTests.cs
@@ -1,13 +1,19 @@
 using System;
 using MbUnit.Framework;
+using Incremental.Kick.Dal;
 
 namespace Incremental.Kick.Tests {
     [TestFixture]
     public class SanityTests {
 
         [Test]
-        public void TheUniverseWorks() {
+        public void TheUniverseWorksTest() {
             Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void CanAccessDatabaseTest() {
+            Assert.GreaterEqualThan(User.GetTotalCount(), 0);
         }
     }
 }
