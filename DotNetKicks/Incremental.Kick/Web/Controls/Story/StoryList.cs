71dc934a37826dcd233a00bb2b132dfdb603a4c4
james.e.welch <james.e.welch@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-21 03:19:03 +0000

    updated StoryList and StorySummary to have ShowFullSummary property. Default=true. When false, then shows a mini-version for Zeitgeist page. Still needs to be cleaned up along with StorySummary full to make it more CSS like, etc. Issue 73

--- DotNetKicks/Incremental.Kick/Web/Controls/Story/StoryList.cs
+++ DotNetKicks/Incremental.Kick/Web/Controls/Story/StoryList.cs
@@ -7,34 +7,78 @@ using Incremental.Kick.Web.Helpers;
 
 namespace Incremental.Kick.Web.Controls {
     public class StoryList : KickWebControl {
-        private StoryCollection _stories;
+        
+		#region [rgn] Fields (3)
 
-        private string _title = "";
-        public string Title {
-            get { return this._title; }
-            set { this._title = value; }
-        }
-
-        //default caption
+		//default caption
         private string _caption = string.Format(
             @"There are currently no stories here.<br/><br/>Would you like to be the first to <a href=""{0}"">submit a story</a>?", 
             UrlFactory.CreateUrl(UrlFactory.PageName.SubmitStory));
+		private StoryCollection _stories;
+		private string _title = "";
+        private bool _showFullSummary = true;
 
+		#endregion [rgn]
+
+		#region [rgn] Properties (2)
+
+		/// <summary>
+        /// Gets or sets the no stories caption.
+        /// </summary>
+        /// <value>The no stories caption.</value>
         public string NoStoriesCaption
         {
             get { return this._caption; }
             set { this._caption = value; }
         }
-        //private StoryListHeader _header = new StoryListHeader();
-        //public StoryListHeader Header {
-        //    get { return this._header; }
-        //}
+		
+		/// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>The title.</value>
+        public string Title {
+            get { return this._title; }
+            set { this._title = value; }
+        }
+		
+		#endregion [rgn]
 
+		#region [rgn] Methods (2)
+
+		// [rgn] Public Methods (1)
+
+
+        /// <summary>
+        /// Binds the data
+        /// </summary>
+        /// <param name="stories">The stories.</param>
         public void DataBind(StoryCollection stories) {
             this._stories = stories;
         }
 
-        protected override void Render(HtmlTextWriter writer) {
+        /// <summary>
+        /// Gets or sets a value indicating whether [show full summary].
+        /// </summary>
+        /// <value><c>true</c> if [show full summary]; otherwise, <c>false</c>.</value>
+        /// <remarks>
+        /// If <c>true</c> then displays the full story summary as shown in upcoming & published story lists.
+        /// If <c>false</c> then displays a shorter story summary as shown in Zeitgeist.
+        /// Default is <c>true</c>.
+        /// </remarks>
+        public bool ShowFullSummary
+        {
+            get { return _showFullSummary; }
+            set { _showFullSummary = value; }
+        }
+      
+
+		// [rgn] Protected Methods (1)
+
+        /// <summary>
+        /// Renders the control to the specified HTML writer.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter"></see> object that receives the control content.</param>
+		protected override void Render(HtmlTextWriter writer) {
 
             if (!String.IsNullOrEmpty(this.Title)) {
                 writer.WriteLine(@"<div class=""PageSmallCaption"">{0}</div>", this.Title);
@@ -48,6 +92,7 @@ namespace Incremental.Kick.Web.Controls {
                 StorySummary story = new StorySummary();
                 this.Controls.Add(story);
                 story.DataBind(storyRow, isOddRow);
+                story.ShowFullSummary = _showFullSummary;
                 story.RenderControl(writer);
                 isOddRow = !isOddRow;
             }
@@ -61,7 +106,8 @@ namespace Incremental.Kick.Web.Controls {
                 ", this.KickPage.StaticIconRootUrl, _caption);
             }
         }
+		
+		#endregion [rgn]
 
-        
     }
 }
