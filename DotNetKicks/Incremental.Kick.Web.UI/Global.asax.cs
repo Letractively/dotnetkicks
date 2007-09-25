5dcec5305425ea0e0009334942c41915ca07d436
simone.busoli <simone.busoli@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-25 23:08:25 +0000

    Reformatted code in Global.asax.cs and added comment. We should comment more, Ohloh says DNK source code as too few comments.

--- DotNetKicks/Incremental.Kick.Web.UI/Global.asax.cs
+++ DotNetKicks/Incremental.Kick.Web.UI/Global.asax.cs
@@ -1,31 +1,22 @@
 using System;
-using System.Data;
-using System.Configuration;
-using System.Collections;
-using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace Incremental.Kick.Web.UI {
     public class Global : System.Web.HttpApplication {
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e) {
-            if (Request.PhysicalPath.EndsWith(".aspx") || Request.PhysicalPath.EndsWith(".axd") || Request.PhysicalPath.EndsWith(".ashx"))
+            if (Request.PhysicalPath.EndsWith(".aspx") || 
+                Request.PhysicalPath.EndsWith(".axd") ||
+                Request.PhysicalPath.EndsWith(".ashx"))
                 Incremental.Kick.Web.Security.SecurityManager.SetPrincipal();
         }
 
         protected void Application_Error(object sender, EventArgs e) {
-            System.Diagnostics.Debug.WriteLine("Application_Error:" + Context.Error.GetBaseException().ToString());
-            if (!this.Request.Url.ToString().ToLower().Contains("http://localhost") && !this.Request.Url.ToString().ToLower().Contains("/services/ajax")) 
+            System.Diagnostics.Debug.WriteLine("Application_Error:" + Context.Error.GetBaseException());
+            
+            // Redirect to error page only if not running locally and if it's not an ajax call
+            if (!Request.Url.ToString().ToLower().Contains("http://localhost") && 
+                !Request.Url.ToString().ToLower().Contains("/services/ajax")) 
                 Response.Redirect("~/error");
-        }
-
-        protected void Application_Start(object sender, EventArgs e) {
-
-        }
-
-        protected void Application_End(object sender, EventArgs e) {
-
         }
     }
 }
