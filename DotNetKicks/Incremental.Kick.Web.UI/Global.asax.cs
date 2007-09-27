8dc9db1266a6c19959f999baa91314df83045863
azizatif <azizatif@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-27 17:09:38 +0000

    Fixed custom error page handling to not use re-direction so that user agents don't get false positives. Issue 122

--- DotNetKicks/Incremental.Kick.Web.UI/Global.asax.cs
+++ DotNetKicks/Incremental.Kick.Web.UI/Global.asax.cs
@@ -1,6 +1,16 @@
-using System;
-
 namespace Incremental.Kick.Web.UI {
+    
+    #region Imports
+
+    using System;
+    using System.Globalization;
+    using System.Web;
+    using System.Web.Hosting;
+    using Debug = System.Diagnostics.Debug;
+    using HttpStatusCode = System.Net.HttpStatusCode;
+
+    #endregion
+
     public class Global : System.Web.HttpApplication {
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e) {
@@ -11,12 +21,39 @@ namespace Incremental.Kick.Web.UI {
         }
 
         protected void Application_Error(object sender, EventArgs e) {
-            System.Diagnostics.Debug.WriteLine("Application_Error:" + Context.Error.GetBaseException());
-            
-            // Redirect to error page only if not running locally and if it's not an ajax call
-            if (!Request.Url.ToString().ToLower().Contains("http://localhost") && 
-                !Request.Url.ToString().ToLower().Contains("/services/ajax")) 
-                Response.Redirect("~/error");
+            const string docsPath = "~/Pages/Docs/";
+            const string defaultErrorPagePath = docsPath + "Error.aspx";
+            const string errorPagePathFormat = docsPath + "Error{0}.aspx";
+
+            Exception error = Server.GetLastError();
+            Debug.WriteLine("Application_Error: " + error.GetBaseException());
+
+            if (!Context.IsCustomErrorEnabled ||
+                Request.Url.ToString().ToLowerInvariant().Contains("/services/ajax"))
+                return;
+
+            string errorPagePath = defaultErrorPagePath;
+            int statusCode = (int) HttpStatusCode.InternalServerError;
+
+            HttpException httpError = error as HttpException;
+            if (httpError != null) 
+            {
+                statusCode = httpError.GetHttpCode();
+
+                string customPage = string.Format(errorPagePathFormat, 
+                    statusCode.ToString(CultureInfo.InvariantCulture));
+
+                if (HostingEnvironment.VirtualPathProvider.FileExists(customPage))
+                    errorPagePath = customPage;
+            }
+
+            Server.Execute(errorPagePath);
+
+            // Server.Execute resets the status code so we need to restore
+            // the actual error once it is done.
+            Response.StatusCode = statusCode;
+
+            Server.ClearError();
         }
     }
 }
