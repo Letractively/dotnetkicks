4d0eb78f1c1ee31356e02faec5b07b2231b244a9
azizatif <azizatif@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-27 08:11:13 +0000

    Re-factored AsyncHelper.FireAndForget to do more with less and using stronger signatures (providing stronger compile-time guarantees) so that a generic Delegate with variable parameters along with superfluous delegate type definitions at the call sites becomes unnecessary.

--- DotNetKicks/Incremental.Kick/Helpers/AsyncHelper.cs
+++ DotNetKicks/Incremental.Kick/Helpers/AsyncHelper.cs
@@ -5,29 +5,14 @@ using System.Threading;
 
 namespace Incremental.Kick.Helpers {
     public class AsyncHelper {
-        class TargetInfo {
-            internal TargetInfo(Delegate d, object[] args) {
-                Target = d;
-                Args = args;
-            }
-
-            internal readonly Delegate Target;
-            internal readonly object[] Args;
-        }
-
-        private static WaitCallback dynamicInvokeShim = new WaitCallback(DynamicInvokeShim);
-
-        public static void FireAndForget(Delegate d, params object[] args) {
-            ThreadPool.QueueUserWorkItem(dynamicInvokeShim, new TargetInfo(d, args));
-        }
-
-        static void DynamicInvokeShim(object o) {
-            try {
-                TargetInfo ti = (TargetInfo)o;
-                ti.Target.DynamicInvoke(ti.Args);
-            } catch (Exception ex) {
-                System.Diagnostics.Trace.WriteLine(ex.ToString());
-            }
+        public static void FireAndForget(ThreadStart work) {
+            ThreadPool.QueueUserWorkItem(delegate {
+                try {
+                    work();
+                } catch (Exception ex) {
+                    System.Diagnostics.Trace.WriteLine(ex.ToString());
+                }
+            });
         }
     }
 }
