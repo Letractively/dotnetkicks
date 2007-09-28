0883f83fdfe6c1f8e090da5a503d3ce7540df5b6
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-28 19:54:29 +0000

    Added some user authorisation unit tests to the Web.UI test suite Issue 8

--- trunk/DotNetKicks/Incremental.Kick.Web.UI.Tests/BrowserActions.cs
+++ trunk/DotNetKicks/Incremental.Kick.Web.UI.Tests/BrowserActions.cs
@@ -19,16 +19,45 @@ namespace Incremental.Kick.Web.UI.Tests {
         }
 
         public DnkBrowser Login(string username, string password) {
-            this.GoTo(_rootUrl + "login");
+            this.GoToLogin();
             this.TextField(Find.ByName(new Regex("Username"))).TypeText(username);
             this.TextField(Find.ByName(new Regex("Password"))).TypeText(password);
             this.Button(Find.ByName(new Regex("LogIn"))).Click();
             return this;
         }
 
+        public DnkBrowser AdminLogin() {
+            return this.Login("admin", "admin");
+        }
+        public DnkBrowser ModeratorLogin() {
+            return this.Login("moderator", "moderator");
+        }
+        public DnkBrowser UserLogin() {
+            return this.Login("user1", "user1");
+        }
+
         public DnkBrowser Logout() {
             this.GoTo(_rootUrl + "logout");
             return this;
+        }
+
+        public DnkBrowser GoToLogin() {
+            this.GoTo(_rootUrl + "login");
+            return this;
+        }
+
+        public DnkBrowser GoToAdmin() {
+            this.GoTo(_rootUrl + "admin");
+            return this;
+        }
+
+
+        public bool IsAdminPage {
+            get { return this.Url.EndsWith("/admin"); }
+        }
+
+        public bool IsNotAuthorisedPage {
+            get { return this.Url.EndsWith("/notauthorised"); }
         }
     }
 }
