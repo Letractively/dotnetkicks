03cf78d0b6309faa6e1cf167ab8e2d5240f9a66f
simone.busoli <simone.busoli@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-29 01:18:22 +0000

    Updated DBBuild.bat to point to the exact location (recently changed) of automatically generated classes.

--- DotNetKicks/Build/DBBuild.bat
+++ DotNetKicks/Build/DBBuild.bat
@@ -7,7 +7,7 @@ set WORKING_DIRECTORY=%CD%
 cd "../3rdParty/SubSonic/186/"
 set SUBSONIC_DIRECTORY=%cd%
 
-sonic.exe generate /config sonic.exe.config  /out %WORKING_DIRECTORY%\Incremental.Kick\Dal\Generated
+sonic.exe generate /config sonic.exe.config  /out %WORKING_DIRECTORY%\Incremental.Kick\Dal\Subsonic\Generated
 
 
 cd %BUILD_DIRECTORY%
