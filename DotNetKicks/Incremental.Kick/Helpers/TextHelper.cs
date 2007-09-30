b781585ef11ba935e29673aa04eec625ba1818ee
simone.busoli <simone.busoli@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-09-30 00:22:36 +0000

    Fixed issue 123: Bug: Url ending in a '.' is not 'Urlified' correctly Replacement algorithm was incorrect. Unit tests pass now. Issue 123

--- DotNetKicks/Incremental.Kick/Helpers/TextHelper.cs
+++ DotNetKicks/Incremental.Kick/Helpers/TextHelper.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
 
@@ -8,30 +6,23 @@ namespace Incremental.Kick.Helpers {
     public class TextHelper {
         public static string Urlify(string input) {
             //NOTE: GJ: this regex fails 'period at the end of a link' unit test
-            return RegExReplace(input, @"((ht|f)tp(s?))\://([0-9a-zA-Z\-]+\.)+[a-zA-Z]{2,6}(\:[0-9]+)?(/\S*)?", @"<a href=""{0}"" target=""_new"">{0}</a>");
+            //return RegExReplace(input, @"((ht|f)tp(s?))\://([0-9a-zA-Z\-]+\.)+[a-zA-Z]{2,6}(\:[0-9]+)?(/\S*)?", @"<a href=""{0}"" target=""_new"">{0}</a>");
 
-            //NOTE: GJ: this regex fails 'two links on a line' unit test            
-            //return RegExReplace(input, @"(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?", @"<a href=""{0}"" target=""_new"">{0}</a>");
+            return RegExReplace(input, @"(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?", @"<a href=""{0}"" target=""_new"">{0}</a>");
         }
 
         public static string RegExReplace(string input, string regExPattern, string outputPattern) {
             return RegExReplace(input, regExPattern, outputPattern, 0);
         }
+
         public static string RegExReplace(string input, string regExPattern, string outputPattern, int matchGroup) {
-            Regex urlMatchRegex = new Regex(regExPattern, RegexOptions.IgnoreCase);
-            MatchCollection urlMatches = urlMatchRegex.Matches(input);
-
-            foreach (Match match in urlMatches) {
-                input = input.Replace(match.Value, String.Format(outputPattern, match.Groups[matchGroup].Value));
-            }
-
-            return input;
+            return  new Regex(regExPattern, RegexOptions.IgnoreCase).Replace(input, delegate(Match match) { return string.Format(outputPattern, match.Value); });
         }
 
         public static string EncodeAndReplaceComment(string message) {
             message = HttpUtility.HtmlEncode(message);
-            message = TextHelper.Urlify(message);
-            message = message.Replace("\n", "<br/>");
+            message = Urlify(message);
+            message = message.Replace("\n", "<br />");
             //TODO: GJ: add reg ex replacements here
             return message;
         }
