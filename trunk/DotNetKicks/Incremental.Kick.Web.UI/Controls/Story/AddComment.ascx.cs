318e000ce9ece9ce6019eca61a52ea0c05666f92
azizatif <azizatif@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-10-02 11:15:24 +0000

    Introduced constants for min and max comment length and made a few small re-factorings while at it.

--- trunk/DotNetKicks/Incremental.Kick.Web.UI/Controls/Story/AddComment.ascx.cs
+++ trunk/DotNetKicks/Incremental.Kick.Web.UI/Controls/Story/AddComment.ascx.cs
@@ -1,14 +1,17 @@
-using System;
-using Incremental.Kick.Caching;
-using Incremental.Kick.Helpers;
-using Incremental.Kick.Web.Controls;
-using Incremental.Kick.Web.Helpers;
-
 namespace Incremental.Kick.Web.UI.Controls
 {
+    using System;
+    using Incremental.Kick.Caching;
+    using Incremental.Kick.Helpers;
+    using Incremental.Kick.Web.Controls;
+    using Incremental.Kick.Web.Helpers;
+
     public partial class AddComment : KickUserControl
     {
         private int _storyID;
+
+        protected const int MinLength = 4;
+        protected const int MaxLength = 2500;
 
         protected string LoginUrl
         {
@@ -27,42 +30,42 @@ namespace Incremental.Kick.Web.UI.Controls
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(KickPage.IsAuthenticated)
+            if (!IsPostBack)
             {
-                AddCommentPanel.Visible = true;
-                LoginToCommentPanel.Visible = false;
+                CommentLength.Text = string.Format(CommentLength.Text,
+                    MinLength.ToString("N0"), MaxLength.ToString("N0"));
             }
-            else
-            {
-                AddCommentPanel.Visible = false;
-                LoginToCommentPanel.Visible = true;
-            }
+
+            bool authenticated = KickPage.IsAuthenticated;
+            AddCommentPanel.Visible = authenticated;
+            LoginToCommentPanel.Visible = !authenticated;
         }
 
         protected void AddComment_Click(object sender, EventArgs e)
         {
-            if(Page.IsValid)
-            {
-                string comment = Comment.Text.Trim();
+            if (!Page.IsValid)
+                return;
 
-                comment = TextHelper.EncodeAndReplaceComment(comment);
-                comment = TextHelper.ReplaceEmoticons(comment, KickPage.StaticEmoticonsRootUrl);
+            string comment = Comment.Text.Trim();
 
-                int commentID =
-                    Dal.Comment.CreateComment(KickPage.HostProfile.HostID, _storyID, KickPage.KickUserProfile, comment);
+            comment = TextHelper.EncodeAndReplaceComment(comment);
+            comment = TextHelper.ReplaceEmoticons(comment, KickPage.StaticEmoticonsRootUrl);
 
-                //now clear the cache for this story (NOTE: in the future, we can just update the cache too)
-                StoryCache.RemoveStory(_storyID, KickPage.UrlParameters.StoryIdentifier);
+            int commentID =
+                Dal.Comment.CreateComment(KickPage.HostProfile.HostID, _storyID, KickPage.KickUserProfile, comment);
 
-                Response.Redirect(
-                    UrlFactory.CreateUrl(UrlFactory.PageName.ViewStory, KickPage.UrlParameters.StoryIdentifier,
-                                         KickPage.UrlParameters.CategoryIdentifier, commentID));
-            }
+            //now clear the cache for this story (NOTE: in the future, we can just update the cache too)
+            StoryCache.RemoveStory(_storyID, KickPage.UrlParameters.StoryIdentifier);
+
+            Response.Redirect(
+                UrlFactory.CreateUrl(UrlFactory.PageName.ViewStory, KickPage.UrlParameters.StoryIdentifier,
+                                     KickPage.UrlParameters.CategoryIdentifier, commentID));
         }
 
         protected void CommentLength_ServerValidate(object source, System.Web.UI.WebControls.ServerValidateEventArgs args)
         {
-            args.IsValid = args.Value.Trim().Length >= 4 && args.Value.Trim().Length <= 2500;
+            string value = (args.Value ?? string.Empty).Trim();
+            args.IsValid = value.Length >= MinLength && value.Length <= MaxLength;
         }
     }
 }
