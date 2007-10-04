20efde3ecaaebcb304cf97e6f81970eda18ba8fb
james.e.welch <james.e.welch@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-10-04 00:16:44 +0000

    Changed length parameter of Incremental.Kick.Helpers.PasswordGenerator.Generate from int32 to byte due to failures of unit tests. int32 allowed negative values (which resulted in errors in code) and int.MaxValue resulted in outofmemory exception. web site currently uses only 8 characters for password so lengths of 0-255 should be acceptable. Also renamed PasswordGeneratorTest to Tests to fit other test files.

---
+++ DotNetKicks/Incremental.Kick.Tests/HelperTests/PasswordGeneratorTests.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Incremental.Kick.Helpers;
+using MbUnit.Framework;
+
+namespace Incremental.Kick.Tests.HelperTests
+{
+    [TestFixture]
+    public class PasswordGeneratorTests
+    {
+
+        [Row(0)]              //valid test
+        [Row(1)]              //valid test
+        [Row(64)]             //valid test
+        [Row(byte.MaxValue)]  //valid test
+        [Row(byte.MinValue)]  //valid test
+        [RowTest]
+        public void GenerateTest(byte length)
+        {
+            // jw - recommend changing int param to byte to prevent outofmemory exceptions errors with negative values
+            string pw = PasswordGenerator.Generate(length);
+            Assert.AreEqual(length, pw.Length);
+        }
+    }
+}
