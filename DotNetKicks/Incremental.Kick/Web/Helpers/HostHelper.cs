ed3f15fce1e0257bd04e7f0bb97d0036c704f186
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-10-05 10:50:49 +0000

    Fixed HostHelper.GetHostName to correctly return hosts minus the '.www'. All tests pass now.

--- DotNetKicks/Incremental.Kick/Web/Helpers/HostHelper.cs
+++ DotNetKicks/Incremental.Kick/Web/Helpers/HostHelper.cs
@@ -5,25 +5,14 @@ using System.Text;
 namespace Incremental.Kick.Web.Helpers {
     public class HostHelper {
         public static string GetHostName(Uri uri) {
-           //NOTE: GJ: This needs to be fixed up with a RegEx, there are a number of failing tests to demonstrate how it should work
-           //The GetHostName should return the full host minus the 'www.'. This is so www.dotnetkicks.com:80 and dotnetkicks.com:80 will resolve to the same Host row in the db. When I wrote this I just needed to get it working with my site, and obviously hacked this together.
-
-           //NOTE: GJ: remove any subdomains
-            string[] segments = uri.Host.Split(".".ToCharArray());
-            if (segments.Length >= 2) {
-                //System.Diagnostics.Trace.WriteLine(segments[segments.Length - 2] + "." + segments[segments.Length - 1]);
-
-                return segments[segments.Length - 2] + "." + segments[segments.Length - 1];
-            } else {
-                //System.Diagnostics.Trace.WriteLine("segments.Length:" + segments.Length);
-
+            if(uri.Host.StartsWith("www."))
+                return uri.Host.Substring(4, uri.Host.Length-4);
+            else
                 return uri.Host;
-            }
         }
 
         public static string GetHostAndPort(Uri uri) {
             return GetHostName(uri) + ":" + uri.Port;
         }
     }
-
 }
