75ed826397e3d2100044ed7f81d51838d2a8c3e0
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-10-05 09:25:37 +0000

    Removed paged API operations in favour of default values. NOTE: This is a breaking change!! Issue 146

--- trunk/DotNetKicks/Incremental.Kick.Web.UI/Services/Json/JsonServices.ashx.cs
+++ trunk/DotNetKicks/Incremental.Kick.Web.UI/Services/Json/JsonServices.ashx.cs
@@ -16,38 +16,20 @@ namespace Incremental.Kick.Web.UI.Services.Json {
     public class JsonServices : KickJsonRpcHandler {
         [JsonRpcMethod("getFrontPageStories", Idempotent = true)]
         [JsonRpcHelp("Returns a list of recently published stories to the homepage")]
-        public ApiPagedList<ApiStory> GetFrontPageStories() {
-            return Story.Api.GetFrontPageStories(this.HostProfile.HostID);
-        }
-
-        [JsonRpcMethod("getFrontPageStoriesPaged", Idempotent = true)]
-        [JsonRpcHelp("Returns a paged list of recently published stories to the homepage")]
-        public ApiPagedList<ApiStory> GetFrontPageStoriesPaged(int pageNumber, int pageSize) {
-            return Story.Api.GetFrontPageStoriesPaged(this.HostProfile.HostID, pageNumber, pageSize);
+        public ApiPagedList<ApiStory> GetFrontPageStories(int pageNumber, int pageSize) {
+            return Story.Api.GetFrontPageStories(this.HostProfile.HostID, pageNumber, pageSize);
         }
 
         [JsonRpcMethod("getUpcomingPageStories", Idempotent = true)]
-        [JsonRpcHelp("Returns a list of recently submitted stories to the site")]
-        public ApiPagedList<ApiStory> GetUpcomingPageStories() {
-            return Story.Api.GetUpcomingPageStories(this.HostProfile.HostID);
-        }
-
-        [JsonRpcMethod("getUpcomingPageStoriesPaged", Idempotent = true)]
         [JsonRpcHelp("Returns a paged list of recently submitted stories to the site")]
-        public ApiPagedList<ApiStory> GetUpcomingPageStoriesPaged(int pageNumber, int pageSize) {
-            return Story.Api.GetUpcomingPageStoriesPaged(this.HostProfile.HostID, pageNumber, pageSize);
+        public ApiPagedList<ApiStory> GetUpcomingPageStories(int pageNumber, int pageSize) {
+            return Story.Api.GetUpcomingPageStories(this.HostProfile.HostID, pageNumber, pageSize);
         }
 
         [JsonRpcMethod("getPopularStories", Idempotent = true)]
-        [JsonRpcHelp("Returns a list of the most popular published stories from the last 30 days")]
-        public ApiPagedList<ApiStory> GetPopularStories() {
-            return Story.Api.GetPopularStories(this.HostProfile.HostID);
-        }
-
-        [JsonRpcMethod("getPopularStoriesPaged", Idempotent = true)]
         [JsonRpcHelp("Returns a paged list of the most popular published stories from the last 30 days")]
-        public ApiPagedList<ApiStory> GetPopularStoriesPaged(int pageNumber, int pageSize) {
-            return Story.Api.GetPopularStoriesPaged(this.HostProfile.HostID, pageNumber, pageSize);
+        public ApiPagedList<ApiStory> GetPopularStories(int pageNumber, int pageSize) {
+            return Story.Api.GetPopularStories(this.HostProfile.HostID, pageNumber, pageSize);
         }
 
         [JsonRpcMethod("getPopularStoriesPagedFromTimePeriod", Idempotent = true)]
@@ -57,15 +39,9 @@ namespace Incremental.Kick.Web.UI.Services.Json {
         }
 
         [JsonRpcMethod("getUpcomingStories", Idempotent = true)]
-        [JsonRpcHelp("Returns a list of the most popular upcoming stories from the last 30 days")]
-        public ApiPagedList<ApiStory> GetUpcomingStories() {
-            return Story.Api.GetUpcomingStories(this.HostProfile.HostID);
-        }
-
-        [JsonRpcMethod("getUpcomingStoriesPaged", Idempotent = true)]
         [JsonRpcHelp("Returns a paged list of the most popular upcoming stories from the last 30 days")]
-        public ApiPagedList<ApiStory> GetUpcomingStoriesPaged(int pageNumber, int pageSize) {
-            return Story.Api.GetUpcomingStoriesPaged(this.HostProfile.HostID, pageNumber, pageSize);
+        public ApiPagedList<ApiStory> GetUpcomingStories(int pageNumber, int pageSize) {
+            return Story.Api.GetUpcomingStories(this.HostProfile.HostID, pageNumber, pageSize);
         }
 
         [JsonRpcMethod("getUpcomingStoriesPagedFromTimePeriod", Idempotent = true)]
@@ -75,64 +51,33 @@ namespace Incremental.Kick.Web.UI.Services.Json {
         }
 
         [JsonRpcMethod("getUserKickedStories", Idempotent = true)]
-        [JsonRpcHelp("Returns a list of the most recent stories kicked by a user")]
-        public ApiPagedList<ApiStory> GetUserKickedStories(string username) {
-            return Story.Api.GetUserKickedStories(this.HostProfile.HostID, username);
-        }
-
-        [JsonRpcMethod("getUserKickedStoriesPaged", Idempotent = true)]
         [JsonRpcHelp("Returns a paged list of the most recent stories kicked by a user")]
-        public ApiPagedList<ApiStory> GetUserKickedStoriesPaged(string username, int pageNumber, int pageSize) {
-            return Story.Api.GetUserKickedStoriesPaged(this.HostProfile.HostID, username, pageNumber, pageSize);
+        public ApiPagedList<ApiStory> GetUserKickedStories(string username, int pageNumber, int pageSize) {
+            return Story.Api.GetUserKickedStories(this.HostProfile.HostID, username, pageNumber, pageSize);
         }
 
         [JsonRpcMethod("getUserSubmittedStories", Idempotent = true)]
-        [JsonRpcHelp("Returns a list of the most recent stories submitted by a user")]
-        public ApiPagedList<ApiStory> GetUserSubmittedStories(string username) {
-            return Story.Api.GetUserSubmittedStories(this.HostProfile.HostID, username);
-        }
-
-        [JsonRpcMethod("getUserSubmittedStoriesPaged", Idempotent = true)]
         [JsonRpcHelp("Returns a paged list of the most recent stories submitted by a user")]
-        public ApiPagedList<ApiStory> GetUserSubmittedStoriesPaged(string username, int pageNumber, int pageSize) {
-            return Story.Api.GetUserSubmittedStoriesPaged(this.HostProfile.HostID, username, pageNumber, pageSize);
+        public ApiPagedList<ApiStory> GetUserSubmittedStories(string username, int pageNumber, int pageSize) {
+            return Story.Api.GetUserSubmittedStories(this.HostProfile.HostID, username, pageNumber, pageSize);
         }
 
         [JsonRpcMethod("getUserFriendsKickedStories", Idempotent = true)]
-        [JsonRpcHelp("Returns a list of the most recent stories kicked by a user's friends")]
-        public ApiPagedList<ApiStory> GetUserFriendsKickedStories(string username) {
-            return Story.Api.GetUserFriendsKickedStories(this.HostProfile.HostID, username);
-
-        }
-
-        [JsonRpcMethod("getUserFriendsKickedStoriesPaged", Idempotent = true)]
         [JsonRpcHelp("Returns a paged list of the most recent stories kicked by a user's friends")]
-        public ApiPagedList<ApiStory> GetUserFriendsKickedStoriesPaged(string username, int pageNumber, int pageSize) {
-            return Story.Api.GetUserFriendsKickedStoriesPaged(this.HostProfile.HostID, username, pageNumber, pageSize);
+        public ApiPagedList<ApiStory> GetUserFriendsKickedStories(string username, int pageNumber, int pageSize) {
+            return Story.Api.GetUserFriendsKickedStories(this.HostProfile.HostID, username, pageNumber, pageSize);
         }
 
         [JsonRpcMethod("getUserFriendsSubmittedStories", Idempotent = true)]
-        [JsonRpcHelp("Returns a list of the most recent stories submitted by a user's friends")]
-        public ApiPagedList<ApiStory> GetUserFriendsSubmittedStories(string username) {
-            return Story.Api.GetUserFriendsSubmittedStories(this.HostProfile.HostID, username);
-        }
-
-        [JsonRpcMethod("getUserFriendsSubmittedStoriesPaged", Idempotent = true)]
         [JsonRpcHelp("Returns a paged list of the most recent stories submitted by a user's friends")]
-        public ApiPagedList<ApiStory> GetUserFriendsSubmittedStoriesPaged(string username, int pageNumber, int pageSize) {
-            return Story.Api.GetUserFriendsSubmittedStoriesPaged(this.HostProfile.HostID, username, pageNumber, pageSize);
+        public ApiPagedList<ApiStory> GetUserFriendsSubmittedStories(string username, int pageNumber, int pageSize) {
+            return Story.Api.GetUserFriendsSubmittedStories(this.HostProfile.HostID, username, pageNumber, pageSize);
         }
 
         [JsonRpcMethod("getTaggedStories", Idempotent = true)]
-        [JsonRpcHelp("Returns a list of the most recent stories tagged with a tag")]
-        public ApiPagedList<ApiStory> GetTaggedStories(string tag) {
-            return Story.Api.GetTaggedStories(this.HostProfile.HostID, tag);
-        }
-
-        [JsonRpcMethod("getTaggedStoriesPaged", Idempotent = true)]
         [JsonRpcHelp("Returns a paged list of the recent stories tagged with a tag")]
-        public ApiPagedList<ApiStory> GetTaggedStoriesPaged(string tag, int pageNumber, int pageSize) {
-            return Story.Api.GetTaggedStoriesPaged(this.HostProfile.HostID, tag, pageNumber, pageSize);
+        public ApiPagedList<ApiStory> GetTaggedStories(string tag, int pageNumber, int pageSize) {
+            return Story.Api.GetTaggedStories(this.HostProfile.HostID, tag, pageNumber, pageSize);
         }
     }
 }
