16fe0a89ef73519d61fdd1dfe3ecb4c20bbd4f5d
james.e.welch <james.e.welch@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-10-05 00:06:13 +0000

    Added some unit tests

---
+++ DotNetKicks/Incremental.Kick.Tests/Web/HelperTests/HostHelperTests.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Incremental.Kick.Web.Helpers;
+using MbUnit.Framework;
+
+namespace Incremental.Kick.Tests.Web.HelperTests
+{
+    [TestFixture]
+    public class HostHelperTests
+    {
+
+        [Row("http://www.yahoo.com", "yahoo.com")] //valid test
+        [Row("http://google.com", "google.com")] //valid test
+        [Row("https://www.yahoo.com", "yahoo.com")] //valid test
+        [Row("http://www.yahoo.co.jp/", "yahoo.com")] //valid test
+        [Row("http://us.rd.yahoo.com/homepage/intlpage/text/jp/*http://www.yahoo.co.jp/", "yahoo.com")] //valid test
+        [Row("http://www.google.com/search?client=firefox-a&rls=org.mozilla%3Aen-US%3Aofficial&channel=s&hl=en&q=dotnetkicks&btnG=Google+Search", "google.com")] //valid test
+        [RowTest]
+        public void GetHostNameTest(string uriString, string expectedHostName)
+        {
+            //fails with international tld (.jp, etc.)
+            Uri uri = new Uri(uriString);
+            string hostName = HostHelper.GetHostName(uri);
+            Assert.AreEqual(hostName, expectedHostName);
+        }
+
+        [Row("http://www.yahoo.com", "yahoo.com:80")] //valid test
+        [Row("http://google.com", "google.com:80")] //valid test
+        [Row("https://www.yahoo.com", "yahoo.com:443")] //valid test
+        [Row("http://www.yahoo.co.jp/", "yahoo.com:80")] //valid test
+        [Row("http://google.com:8080", "google.com:8080")] //valid test
+        [Row("http://us.rd.yahoo.com/homepage/intlpage/text/jp/*http://www.yahoo.co.jp/", "yahoo.com:80")] //valid test
+        [Row("http://www.google.com/search?client=firefox-a&rls=org.mozilla%3Aen-US%3Aofficial&channel=s&hl=en&q=dotnetkicks&btnG=Google+Search", "google.com:80")] //valid test
+        [RowTest]
+        public void GetHostAndPortTest(string uriString, string expectedHostAndPort)
+        {
+            //fails with international tld (.jp, etc.)
+            Uri uri = new Uri(uriString);
+            string hostNameAndPort = HostHelper.GetHostAndPort(uri);
+            Assert.AreEqual(hostNameAndPort, expectedHostAndPort);
+        }
+    }
+}
