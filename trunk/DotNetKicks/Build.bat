c808dbe56209f796d9d49f7203760e078d773389
simone.busoli <simone.busoli@42c5ea4f-242c-0410-9959-2d221710c8fd>
2007-10-28 15:16:12 +0000

    Made Build.bat interactive. Users are prompted to choose the build type the want to execute. Issue 9

--- trunk/DotNetKicks/Build.bat
+++ trunk/DotNetKicks/Build.bat
@@ -1,2 +1,19 @@
-%windir%\Microsoft.NET\Framework\v2.0.50727\MSBuild DotNetKicks.msbuild
-PAUSE
+@echo off
+
+ECHO 1: Build and run all tests and analysis
+ECHO 2: Build Only
+ECHO 3: Build and run tests
+ECHO 4: Build and run FxCop analysis
+
+set buildtype=BuildAll
+set /p choice=Choose a build type: 
+
+if %choice% EQU 2 set buildtype=Build
+if %choice% EQU 3 set buildtype=Tests
+if %choice% EQU 4 set buildtype=FxCop
+
+:build 
+echo Building target %buildtype%
+%windir%\Microsoft.NET\Framework\v2.0.50727\MSBuild DotNetKicks.msbuild /t:%buildtype%
+echo Built target %buildtype%
+pause
