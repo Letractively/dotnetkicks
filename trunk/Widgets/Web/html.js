036cfc9dd1664aec043d19d234056dc1281cc41a
azizatif <azizatif@42c5ea4f-242c-0410-9959-2d221710c8fd>
2008-06-26 09:22:20 +0000

    - Added HTML.text() to html.js for creation of text nodes. - Updated other HTML members in html.js to make arguments entirely optional. - Updating index.html to be terser based on above changes.

--- trunk/Widgets/Web/html.js
+++ trunk/Widgets/Web/html.js
@@ -1,27 +1,34 @@
 var HTML = {
     a : function(href, child) {
         var e = document.createElement('a');
-        e.href = href;
-        if (child)
-            e.appendChild(typeof(child) === 'string' ? document.createTextNode(child) : child);
+        if (arguments.length) {
+            e.href = href;
+            if (child)
+                e.appendChild(typeof(child) === 'string' ? this.text(child) : child);
+        }
         return e;
     },
     img : function(src, alt) {
         var e = document.createElement('img');
-        e.src = src;
-        if (alt && alt.length > 0) e.alt = alt;
-        e.border = '0';
+        if (arguments.length) {
+            e.src = src;
+            if (alt && alt.length > 0) e.alt = alt;
+            e.border = '0';
+        }
         return e;
     },
     p : function(child) {
         var e = document.createElement('p');
         if (child)
-            e.appendChild(typeof(child) === 'string' ? document.createTextNode(child) : child);
+            e.appendChild(typeof(child) === 'string' ? this.text(child) : child);
         return e;
     },
     span : function(text) {
         var e = document.createElement('span');
-        e.appendChild(document.createTextNode(text));
+        if (text) e.appendChild(this.text(text));
         return e;
+    },
+    text : function(text) {
+        return document.createTextNode(text);
     }
 };
