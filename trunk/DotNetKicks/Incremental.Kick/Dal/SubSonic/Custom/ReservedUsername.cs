1e8b6e94a80d4bfcb1769b9e95b1e959e1c4f630
gavinjoyce <gavinjoyce@42c5ea4f-242c-0410-9959-2d221710c8fd>
2008-12-10 21:44:00 +0000

    Added missing files (thanks to John Sheehan for pointing out these were missing)

---
+++ trunk/DotNetKicks/Incremental.Kick/Dal/SubSonic/Custom/ReservedUsername.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Incremental.Kick.Dal {
+    public partial class ReservedUsername {
+        //TODO: GJ: refactor to helper class
+        public static bool IsColumnValueUnique(string columnName, object value) {
+            using (IDataReader reader = ReservedUsername.FetchByParameter(columnName, value))
+                return reader.Read();
+        }
+    }
+}
